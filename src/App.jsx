7b7960f6467371c48a9d6e564280ad2a67a901b3
Akshat3128 <akshatsaxena62941@gmail.com>
2025-05-23 00:16:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,32 +18,5 @@ const App = () => {
   </div>)
 };
 
-// export default App;
-// https://github.com/Govind783/react-e-commerce-/tree/main
-
-// import React from "react";
-// import { BrowserRouter as Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Cart from "./pages/Cart";
-// // import PaymentPage from "./pages/PaymentPage"; // Import the PaymentPage
-
-// const App = () => {
-//   return (
-//     <div>
-//       {/* <Router> */}
-//         <div className="bg-slate-900">
-//           <Navbar />
-//         </div>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/cart" element={<Cart />} />
-//           {/* <Route path="/payment" element={<PaymentPage />} /> */}
-//         </Routes>
-//       {/* </Router> */}
-//     </div>
-    
-//   );
-// };
 
 export default App;
